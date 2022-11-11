a18a3b319d79cb4958ac2f031a0869c7292a8e82
GrimMori <armigofire@gmail.com>
2022-11-11 01:19:27 +0100

    ejercicios terminados, falta el bonus

--- tests.js
+++ tests.js
@@ -149,7 +149,7 @@ describe("Operaciones iterables", () => {
         let list = [1,5,7,9,11,13];
 
         // utiliza la función map para aplicar los números de la lista
-        list = "???";
+        list = list.map(n => n-1);
         
 
         expect(list).to.have.same.members([0,4,6,8,10,12]);
@@ -160,7 +160,7 @@ describe("Operaciones iterables", () => {
         let list = ["Pikachu", "Charmander", "Magikarp"];
 
         // utiliza la función map para aplicar los números de la lista y guardar la nueva lista en result
-                
+        let result = list.map(s => s + ", te elijo a ti!")
         expect(result).to.have.same.members([
             "Pikachu, te elijo a ti!",
             "Charmander, te elijo a ti!",
@@ -173,7 +173,7 @@ describe("Operaciones iterables", () => {
         let knownExoplanets = [ "TOI-1298 b","TOI-132 b","TOI-1333 b","TOI-1338 b","TOI-1431 b","TOI-1444 b","TOI-1478 b","TOI-150.01","TOI-157 b","TOI-1601 b","TOI-163 b","TOI-1634 b","TOI-1685 b","TOI-169 b","TOI-172 b","TOI-1728 b","TOI-1749 b","TOI-1749 c"];
 
         // utiliza la función includes para comprobar si la lista de planetas contiene el planeta TOI-1634 b
-        let result = "???";
+        let result = knownExoplanets.includes("TOI-1634 b");
         
         expect(result).to.equal(true);        
     })
@@ -185,14 +185,32 @@ describe("Operaciones iterables", () => {
         // utiliza la función filter para quedarnos con los planetas que terminan por la letra c
         // puedes utilizar la función .endsWith para comprobar si un string termina por una letra
         // p.ej. "Hola".endsWith('a') devuelve true
-        let result = "???";
+
+
+        function terminaEn(str) {
+            if (str.endsWith('c')) {
+                return true;
+            }
+        }
+
+        let result = knownExoplanets.filter( terminaEn );
         
 
         // todos los planetas de result terminan con la letra c
         expect(result.every( p => p.endsWith('c'))).to.equal(true);        
     })
+})
 
     // Bonus: Escribe un test como los anteriores con algún ejemplo que utilice 
     // una lista alguna de las funciones que hemos visto, como map o filter.
-    
+
+describe("BONUS", () => {
+    it("encuentra a yippee", () => {
+        let arrayArbitrario = ["ernesto", "serrallonga", 99, 999, true, undefined, "yippee"]
+
+        // en qué índice del array se encuentra yippee?
+        let result = "???";
+
+        expect(result.to.equal("yippee")); // no me sale
+    })
 })
